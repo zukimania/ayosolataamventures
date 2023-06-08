936b68043bfbdd00cffd17970639f34642634ada
zukimania <eayodele585@gmail.com>
2023-06-08 12:54:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+ function myFunction() {
+            const x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
