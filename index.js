637578a090829518b68a4a80da9c0f5ffe9fbc96
zukimania <eayodele585@gmail.com>
2023-06-12 11:53:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// const the_animation = document.querySelectorAll('.services-img')
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('scroll-animation')
+//         }
+//             else {
+//                 entry.target.classList.remove('scroll-animation')
+//             }
+        
+//     })
+// },
+//    { threshold: 0.5
+//    });
+// //
+//   for (let i = 0; i < the_animation.length; i++) {
+//    const elements = the_animation[i];
+
+//     observer.observe(elements);
+//   }
+
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+    boxes.forEach((box) => {
+          const boxTop = box.getBoundingClientRect().top; if (boxTop < triggerBottom) box.classList.add("show"); else box.classList.remove("stay");    
+}
+    )
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes()
